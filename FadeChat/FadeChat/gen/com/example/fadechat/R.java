62f930648bd4e707987b1eadd70104581509a1ef
ldu1225 <ldu1225@naver.com>
2015-02-16 23:35:18 +0900

    UI _modify
    modify

--- FadeChat/FadeChat/gen/com/example/fadechat/R.java
+++ FadeChat/FadeChat/gen/com/example/fadechat/R.java
@@ -22,9 +22,11 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int message_left=0x7f020001;
-        public static final int message_right=0x7f020002;
+        public static final int fadechat_intro=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int message_left=0x7f020002;
+        public static final int message_right=0x7f020003;
+        public static final int up_icon=0x7f020004;
     }
     public static final class id {
         public static final int action_settings=0x7f080007;
@@ -38,7 +40,8 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int msg_item=0x7f030001;
+        public static final int intro_fadechat=0x7f030001;
+        public static final int msg_item=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- FadeChat/FadeChat/src/com/example/fadechat/MainActivity.java
+++ FadeChat/FadeChat/src/com/example/fadechat/MainActivity.java
@@ -1,18 +1,77 @@
 package com.example.fadechat;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
+	private ListView msgListView;
 
+	private EditText inputText;		//메세지를 입력하는 텍스트
+
+	private Button send;		//send 버튼 
+	
+	private MsgAdapter adapter;	//메세지 받는 변
+
+	private List<Msg> msgList = new ArrayList<Msg>();	//어레이리스트로 생성한 메세지 리스
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+
 		setContentView(R.layout.activity_main);
+		adapter = new MsgAdapter(MainActivity.this, R.layout.msg_item, msgList);
+		
+		initMsgs();
+
+		
+		inputText = (EditText) findViewById(R.id.input_text);
+		send = (Button) findViewById(R.id.send);
+		msgListView = (ListView) findViewById(R.id.msg_list_view);
+		msgListView.setAdapter(adapter);
+		send.setOnClickListener(new OnClickListener() {
+			
+			
+			@Override
+			public void onClick(View v) {
+				
+				String content = inputText.getText().toString();
+				if (!"".equals(content)) {
+					//타입은 sent 로 잡는다. (메세지 보내기)
+					Msg msg = new Msg(content, Msg.TYPE_SENT);
+					msgList.add(msg);	//메세지 추가
+					adapter.notifyDataSetChanged();
+					msgListView.setSelection(msgList.size());
+					inputText.setText("");
+				}
+			}
+		});
+	
+
+	}
+				
+	private void initMsgs() {
+		Msg msg1 = new Msg("Start Fade Chat", Msg.TYPE_RECEIVED);
+		msgList.add(msg1);
+		Msg msg2 = new Msg("Fade Chat 은 당신의 정보를 최우선으로 생각합니다.", Msg.TYPE_SENT);
+		msgList.add(msg2);
 	}
 
 	@Override
